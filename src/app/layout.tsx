cf167f7ca08b204568533e50d7925c5acfe2cb53
Moyez <moyezrabbani@gmail.com>
2023-10-30 18:44:44 +0530

    Initial Commit - Finished Setting up files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import { GeistSans } from "geist/font";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={GeistSans.className}>
+      <body>{children}</body>
     </html>
   )
 }
