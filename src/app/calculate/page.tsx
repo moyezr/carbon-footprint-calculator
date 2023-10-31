1b53859f3e73ea3df7fdb64254a64155c20e6225
Moyez <moyezrabbani@gmail.com>
2023-10-31 23:22:17 +0530

    added select form to the first tab

--- src/app/calculate/page.tsx
+++ src/app/calculate/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import HouseMembers from "@/components/tabs/HouseMembers";
 import {
   Progress,
   Card,
@@ -12,18 +13,19 @@ import {
 import { useState } from "react";
 type Props = {};
 
-const page = (props: Props) => {
+const Calcula = (props: Props) => {
   const [progress, setProgress] = useState<number>(0);
+
+  
+
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col gap-1 text-black w-[80%] md:w-[60%] lg:w-[40%] ">
         <Progress size="md" aria-label="Loading..." value={progress} />
-        <Card className="w-full flex">
-          <CardHeader>How many members do you have in your house?</CardHeader>
-        </Card>
+        {progress == 0 && <HouseMembers />}
       </div>
     </main>
   );
 };
 
-export default page;
+export default Calcula;
